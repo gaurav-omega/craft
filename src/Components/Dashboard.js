91bc9241c1639092cc2fef6ba8838776ffb3f61b
gaurav-omega <movie3gdot@gmail.com>
2023-11-20 15:14:13 +0530

    complete dash as adviced

--- src/Components/Dashboard.js
+++ src/Components/Dashboard.js
@@ -1,24 +1,74 @@
-import React from "react";
+import React, { useState } from "react";
 import { Homeicon } from "./svg/Homeicon";
-import {Ordericon} from './svg/Ordericon'
+import { Ordericon } from "./svg/Ordericon";
 
-const dashboard = ()=>{
-    return(
-        <div className="bg_secondary w-72 h-screen text-white flex flex-col">
-            <div className="my-6 text-2xl font-semibold text-center txt_prime">Craftlooms</div>
-            <hr className="w-full" />
-            <div className="gap-1 flex flex-col text-lg font-semibold mt-6 px-4"> 
-                <div className="flex items-center gap-x-2 drop-shadow-lg button_color">
-                    <div className="mx-2"><Homeicon/></div>
-                    <div>Dashboard</div>
-                </div>
-                <div  className="flex items-center gap-x-2 drop-shadow-lg button_color">
-                    <div className="mx-2"><Ordericon/></div>
-                    <div>Manage Orders</div>
-                </div>
-            </div>
-        </div>
-    )
-}
+const tabs = [
+  { id: "dashboard", label: "Dashboard", icon: <Homeicon /> },
+  {
+    id: "manageOrders",
+    label: "Manage Orders",
+    icon: <Ordericon />,
+    subTabs: [
+      { id: "createOrder", label: "Create Order" },
+      { id: "viewOrders", label: "View Orders" },
+    ],
+  },
+];
 
-export default dashboard;
+const Dashboard = () => {
+  const [activeTab, setActiveTab] = useState("dashboard");
+  const [activeSubTab, setActiveSubTab] = useState(null);
+
+  const handleTabClick = (tabId) => {
+    setActiveTab(tabId);
+    setActiveSubTab(null);
+  };
+
+  const handleSubTabClick = (subTabId) => {
+    setActiveSubTab(subTabId);
+  };
+
+  return (
+    <div className="bg_secondary w-72 h-screen text-white flex flex-col">
+      <div className="my-6 text-2xl font-semibold text-center txt_prime">
+        Craftlooms
+      </div>
+      <hr className="w-full" />
+      <div className="gap-1 flex flex-col text-lg font-semibold mt-6 px-4">
+        {tabs.map((tab) => (
+          <div
+            key={tab.id}
+            className={`flex items-center gap-x-2  list py-3 px-2 active_b ${
+                activeTab === tab.id ? "button_active" : ""
+              }`}
+            onClick={() => handleTabClick(tab.id)}
+          >
+            <div className="mx-2">{tab.icon}</div>
+            <div>{tab.label}</div>
+          </div>
+        ))}
+
+        {activeTab === "manageOrders" && (
+          <div className="flex flex-col text-lg font-semibold mt-3 px-4">
+            {tabs
+              .find((tab) => tab.id === "manageOrders")
+              .subTabs.map((subTab) => (
+                <div
+                  key={subTab.id}
+                  className={`flex items-center gap-x-2 list py-3 px-2 ml-4 active_b ${
+                    activeSubTab === subTab.id ? "button_active" : ""
+                  } `}
+                  onClick={() => handleSubTabClick(subTab.id)}
+                >
+                  <div className="mx-2">{/* Optionally add subTab.icon */}</div>
+                  <div>{subTab.label}</div>
+                </div>
+              ))}
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Dashboard;
